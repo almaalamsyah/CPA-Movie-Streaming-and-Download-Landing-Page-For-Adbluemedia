ae977e57d1208293bfafb17f2830cb4f08dc44bf
Alma Alamsyah <almaalamsyah@gmail.com>
2024-05-10 17:02:05 +0700

    first commit

---
+++ config/config.php
@@ -0,0 +1,19 @@
+<?php  
+
+$Movie_Grp_uri = 'https://'; // Website Link
+$Movie_Grp_wname = 'Movies'; // Website  Title
+$Movie_Grp_descrip = 'Free Movies'; // Website Description
+
+$Movie_Grp_adbluemedia_it = '1444147';  // AdBlueMedia IT
+$Movie_Grp_adbluemedia_key = 'e619c';  // AdBlueMedia KEY
+
+$Movie_Grp_ImdbApi = 'b7cd3340a794e5a2f35e3abb820b497f'; // Themoviedb API
+
+$Movie_Grp_video_mp4 = 'https://cdn.jsdelivr.net/gh/iDevMore/tvs-vd1/nfx.mp4'; // Video Link
+$Movie_Grp_pause_time = '5'; // Time to Stop Movie
+
+$Movie_Grp_comingsoon = '1';  // 1 to display
+$Movie_Grp_related = '1'; // 1 to display
+
+
+?>
---
+++ inc/movie_footer.php
@@ -0,0 +1,25 @@
+    <div class="spaces" ></div>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script> 
+    document.onkeydown = function(e) {
+        if(event.keyCode == 123) {
+            return false;
+        }
+        if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+            return false;
+        }
+        if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+            return false;
+        }
+        if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+            return false;
+        }
+        if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+            return false;
+        }
+    }
+    </script>
+</body>
+</html>
